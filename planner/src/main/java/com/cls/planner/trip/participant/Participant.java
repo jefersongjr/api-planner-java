19c0e45731e62f9a69b7d4174cd9f54f653faac6
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-09 23:46:37 -0300

    feat :sparkles: criando classe  participant///

---
+++ planner/src/main/java/com/cls/planner/trip/participant/Participant.java
@@ -0,0 +1,36 @@
+package com.cls.planner.trip.participant;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "participants")
+public class Participant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private Boolean isConfirmed;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+
+}
