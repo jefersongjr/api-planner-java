9f268a07ca513a37a0fa89bb7e15e1aef5b05046
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-09 22:05:00 -0300

    feat :sparkles: criando o package participant e a ParticipantService

---
+++ planner/src/main/java/com/cls/planner/trip/participant/ParticipantService.java
@@ -0,0 +1,18 @@
+package com.cls.planner.trip.participant;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ParticipantService {
+
+    public void registerPaticipantsToEvent(List<String> participantsToInvite, UUID id) {
+
+    }
+
+    public void triggerConfirmationEmailtoParticipants(UUID tripId) {
+
+    }
+}
